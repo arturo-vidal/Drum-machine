b4c9e9bc5d5efe7b32e137497c2ef5ce1e7001cf
unknown <arturohernandezvidal.ahv@gmail.com>
2022-07-13 21:56:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from './Box';
+
+const sounds=[
+  {
+    key: "Q",
+    id:"Open HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+  },
+  {
+    key: "W",
+    id:"Closed HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  },
+  {
+    key: "E",
+    id:"Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  },
+  {
+    key: "A",
+    id:"Snare",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+  },
+  {
+    key: "S",
+    id:"Clap",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+  {
+    key: "D",
+    id:"Kick & hat",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  },
+  {
+    key: "Z",
+    id:"Shaker",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
+  },
+  {
+    key: "X",
+    id:"Rim",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
+  },
+  {
+    key: "C",
+    id:"Kick 2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
+  }
+];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="drum-machine">
+        <div id="drums">
+          {sounds.map((sound)=>(<Box iD={sound.id} text={sound.key} audio={sound.url}/>))}
+        </div>
+          <div id="container">
+            <div id="display">
+              <h1>DRUM MACHINE</h1>
+            </div>
+            <img src="https://www.pngkey.com/png/full/219-2191333_drum-kit-sprite-007-drum-sprite.png" alt="drimset img"/>
+          </div>
+      </div>
     </div>
   );
 }
---
+++ src/Box.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Box extends React.Component{
+    constructor(props){
+      super(props);
+      this.audio= React.createRef();
+      
+      window.document.addEventListener("keydown", (e)=>{
+        if(e.key.toUpperCase()===this.props.text){
+          this.audio.current.play();
+        }
+      });
+    }
+    
+    playSound=()=>{
+      this.audio.current.play();
+      this.audio.current.parentNode.parentNode.parentNode.querySelector("h1").innerText= this.audio.current.parentNode.id;
+    }
+    
+    render(){
+      return(
+        <div className="drum-pad" id={this.props.iD} onClick={this.playSound}>
+          {this.props.text}
+          <audio className="clip" id={this.props.text} ref={this.audio} src={this.props.audio} />
+        </div>
+      );
+    }  
+  }
+
+export default Box;
